3cf0238cb597b08385283ed0d116b7cfdddf9c96
Szász Kinga <kingus77@gmail.com>
2018-03-09 10:50:33 +0100

    json data handled

--- js/movies.js
+++ js/movies.js
@@ -1,1 +1,17 @@
-console.log('itt');
+function getData(url, callback) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            callback(this);
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+function onSuccess(xhttp) {
+    var movieData = JSON.parse(xhttp.responseText);
+    console.log(movieData);
+}
+
+getData('json/movies.json', onSuccess);
