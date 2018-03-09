a317dab3c224d0cd288d5f339da701a82d2dbb21
Szász Kinga <kingus77@gmail.com>
2018-03-09 13:27:50 +0100

    basic show working

--- js/movies.js
+++ js/movies.js
@@ -11,13 +11,13 @@ function getData(url, callback) {
 
 function onSuccess(xhttp) {
     var movieData = JSON.parse(xhttp.responseText);
-    console.log(movieData);
+    //console.log(movieData);
 
     sortByName(movieData);
     console.log(movieData);
 
-    fixCategoryNames(movieData)
-
+    fixCategoryNames(movieData);
+    generateMovieView(movieData);
 
 }
 
@@ -35,6 +35,7 @@ function sortByName(data) {
         }
     }
 }
+
 
 
 function fixCategoryNames(data) {
@@ -66,8 +67,67 @@ function fixCategoryNames(data) {
 
     for (let i = 0; i < data.movies.length; i++) {
         for (j in data.movies[i].categories) {
-            data.movies[i].categories[j] = data.movies[i].categories[j].replace(/^[a-z]/, c => upperCase[c]);
-            console.log(data.movies[i].categories[j]);
+            data.movies[i].categories[j] =
+                data.movies[i].categories[j].replace(/^[a-z]/, c => upperCase[c]);
         }
     }
+}
+
+
+function removesShit(str) {
+    const hunChar = {
+        á: 'a',
+        é: 'e',
+        í: 'i',
+        ó: 'o',
+        ö: 'o',
+        ő: 'o',
+        ú: 'u',
+        ü: 'u',
+        ű: 'u',
+    }
+    str = str.toLocaleLowerCase();
+    str = str.replace(/[\?:;,\.\+\*_]/g, ''); // [\?:;,\.\+\*_] kicsréli semmire globálisan
+    str = str.replace(/[áéíóöőúüű]/g, c => hunChar[c]); //
+    str = str.replace(/[ -]+/g, '-'); //a szóközt egyszer v többször ha előfordul kicserélem kötőjelre
+
+    return str;
+}
+
+function showMovie(i, data) {
+    let divMovie = `<div class="movie">
+                    <h2> Cím: ${data.movies[i].title}</h2>
+                    <img src="/img/covers/${removesShit(data.movies[i].title)}.jpg" alt="${data.movies[i].title}">
+                    <p>Kategória: ${data.movies[i].categories.join(', ')}</p>
+                    <p>Hossz: ${data.movies[i].timeInMinutes} perc </p>
+                    <p>Premier: ${data.movies[i].premierYear} </p>
+                    <p>Premier: ${data.movies[i].directors.join(', ')}</p>
+                    `;
+
+    return divMovie;
+}
+
+function showActors(i, j, data) {
+    let divCast = `<div class="cast">
+                    <img src="/img/actors/${removesShit(data.movies[i].cast[j].name)}.jpg" alt="${data.movies[i].cast[j].name}">
+                    <p>${data.movies[i].cast[j].name} (${data.movies[i].cast[j].characterName})</p>
+                    <p>${data.movies[i].cast[j].birthYear}, ${data.movies[i].cast[j].birthCountry},${data.movies[i].cast[j].birthCity} </p>
+                    </div>`;
+
+    return divCast;
+}
+
+
+
+function generateMovieView(data) {
+    let container = '';
+    for (let i = 0; i < data.movies.length; i++) {
+        container += showMovie(i, data);
+        container += '<h2>Szereplők</h2>'
+        for (let j = 0; j < data.movies[i].cast.length; j++) {
+            container += showActors(i, j, data);
+        }
+    }
+    container += `</div>`
+    document.getElementsByClassName('container')[0].innerHTML = container;
 }
